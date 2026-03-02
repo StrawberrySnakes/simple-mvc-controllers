9abd03117e5da0820dd7372b82a05ec87ef09ece
DJ <djs9826@rit.edu>
2026-03-02 12:48:21 -0500

    week 8-1 classwork

--- src/app.js
+++ src/app.js
@@ -7,3 +7,19 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const app = express();
+
+
+app.use(compression());
+app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+app.use(favicon(path.resolve(`${__dirname}/../client/img/favicon.ico`)));
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Listening on port ${port}`);
+});
+
+
+
+
+
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -15,3 +15,32 @@ const hostPage1 = (req, res) => {
 const hostPage2 = (req, res) => {
   return res.sendFile(path.resolve(`${__dirname}/../../views/page2.html`));
 };
+
+const notFound = (req, res) => {
+  return res.status(404).sendFile(path.resolve(`${__dirname}/../../views/404.html`));
+}
+
+const getName = (req, res) => {
+  return res.json({ name });
+}
+
+const setName = (req, res) => {
+  console.log(req.body);
+  if(!req.body.firstName || !req.body.lastName) {
+    return res.status(400).json({ 
+      error: "Missing firstName or lastName",
+      id: "setNameMissingParams", 
+    });
+  }
+  name = `${req.body.firstName} ${req.body.lastName}`;
+  return res.json({ name });
+}
+
+
+module.exports = {
+  index: hostIndex,
+  page1: hostPage1,
+  page2: hostPage2,
+  notFound, 
+  getName,
+};
